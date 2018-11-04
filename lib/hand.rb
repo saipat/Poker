eda5af973b72ecdba5e2254dd741b0789319c95e
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2018-11-04 00:06:59 -0700

    Modify hand.rb to pass the specs

--- lib/hand.rb
+++ lib/hand.rb
@@ -6,8 +6,8 @@ class Hand
   attr_reader :cards
 
   def initialize(cards)
-    raise "must have five cards" unless cards.count == 5
-    @cards = cards
+    raise 'must have five cards' unless cards.count == 5
+    @cards = cards.sort
   end
 
   def self.winner(hands)
@@ -15,27 +15,30 @@ class Hand
   end
 
   def trade_cards(old_cards, new_cards)
-    
+    raise 'must have five cards' unless old_cards.count == new_cards.count
+    raise 'cannot discard unowned card' unless has_cards?(old_cards)
+    take_cards(new_cards) && discard_cards(old_cards) && sort!
+    old_cards
   end
 
   def to_s
-
+    cards.join(' ')
   end
 
   private
   def sort!
-
+    @cards.sort!
   end
 
   def take_cards(cards)
-
+    @cards.push(*cards)
   end
 
   def discard_cards(old_cards)
-
+    old_cards.each { |card| cards.delete(card) }
   end
 
   def has_cards?(old_cards)
-
+    old_cards.all? { |card| cards.include?(card) }
   end
 end
